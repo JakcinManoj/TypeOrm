181d4e7a151b2c70a0667d7d6b0b7ba886c85bec
Jakcin <jakcinmanojjacobraj@devaxcel.com>
2023-03-16 10:05:41 +0530

    added id as pk from user table

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,5 +1,6 @@
 import { Entity, PrimaryGeneratedColumn, Column, OneToMany } from "typeorm"
 import { Photo } from "./Photo"
+import { Address } from "./Address"
 
 @Entity()
 export class User {
@@ -9,7 +10,9 @@ export class User {
     @Column()
     name: string
 
-    @OneToMany(() => Photo, (photo) => photo.user)
-    photos: Photo[]
-    addresses: any
+    @OneToMany(() => Photo, (photo) => photo.id)
+    photos: Photo;
+
+    @OneToMany(() => Address, (address) => address.id)
+    addresses: Address;
 }
